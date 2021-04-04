0d27dc44d9be693265bcadaf5528c82e81ca169c
Stas <holidayonmail@gmail.com>
2021-04-04 17:50:16 +0300

    Added stdIN.go

--- stdOut.go
+++ stdOut.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	myString := ""
+	arguments := os.Args
+	if len(arguments) == 1 {
+		myString = "Please give me one argument"
+	} else {
+		myString = arguments[1]
+	}
+	io.WriteString(os.Stdout, myString+"\n")
+	io.WriteString(os.Stderr, "Testing Error output")
+	io.WriteString(os.Stdout, "\n")
+
+}
