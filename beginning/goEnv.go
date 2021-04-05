1e50fe804155f452f5df3e69e7cb4b682f483702
Stas <holidayonmail@gmail.com>
2021-04-05 12:00:22 +0300

    Added gourutines

--- beginning/goEnv.go
+++ beginning/goEnv.go
@@ -7,8 +7,7 @@ import (
 
 func main() {
 	fmt.Print("You are using: ", runtime.Compiler, " ")
-	fmt.Println("on a", runtime.GOARCH, "machine")
-	fmt.Println("Using Go version", runtime.Version())
+	fmt.Println("on a", runtime.GOARCH, "machine"
 	fmt.Println("Number of CPUs: ", runtime.NumCPU())
 	fmt.Println("Number of Goroutines: ", runtime.NumGoroutine())
 }
